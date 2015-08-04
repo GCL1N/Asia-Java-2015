e5c6ba6f9b06cce7b67b7af9f792aeb6564ddc33
Lin Jyun Shih <104021018@live.asia.edu.tw>
2015-08-04 14:03:14 +0800

    debug pratice

---
+++ AsiaJava2015/src/test1.java
@@ -0,0 +1,16 @@
+
+public class test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int va1 = 1 ;
+System.out.println("Hello world!"+ va1);
+String  str= fun1();
+	}
+public static  String fun1(){
+	System.out.println("Fun1");
+	return null;
+				
+	}
+
+}
